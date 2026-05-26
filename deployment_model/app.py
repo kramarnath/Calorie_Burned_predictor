00c9514cf2a99b7e70e2068ed80ff24674a83044
Amarnath K R <182259361+kramarnath@users.noreply.github.com>
2026-05-26 13:35:49 +0530

    Add files via upload

---
+++ deployment_model/app.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import pandas as pd
+import pickle
+
+# LOAD MODEL
+model = pickle.load(open("calorie_model.pkl", "rb"))
+
+# TITLE
+st.title("Calories Burn Prediction")
+
+# USER INPUTS
+gender_text = st.selectbox("Gender",["Male", "Female"])
+age = st.number_input("Age")
+height = st.number_input("Height (cm)",min_value=1.0)
+weight = st.number_input("Weight (kg)")
+duration = st.number_input("Duration (minutes)")
+heart_rate = st.number_input("Heart Rate")
+body_temp = st.number_input("Body Temperature")
+
+# CONVERT GENDER
+gender = 1 if gender_text == "Male" else 0
+
+# BMI FEATURE
+bmi = weight / ((height / 100) ** 2)
+
+# PREDICT BUTTON
+if st.button("Predict Calories"):
+
+    input_df = pd.DataFrame([{
+        'Gender': gender,
+        'Age': age,
+        'Height': height,
+        'Weight': weight,
+        'Duration': duration,
+        'Heart_Rate': heart_rate,
+        'Body_Temp': body_temp,
+        'BMI': bmi }])
+
+    prediction = model.predict(input_df)
+    st.success(f"Estimated Calories Burned: {prediction[0]:.2f}")
